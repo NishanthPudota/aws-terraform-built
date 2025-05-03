3542b63ffc79f089446a339183ab03bd88726c00
NishanthPudota <nishanthpudota3860@gmail.com>
2025-05-03 20:12:20 +0530

    removing dynamodb lock

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "aws_terraform_build_version"
     key            = "env/dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
 }
